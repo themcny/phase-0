098d65ceeef8d8d97ba820e8d3c926c46c82939f
unknown <n.lindsey.yee@gmail.com>
2015-09-28 16:52:52 -0700

    Create methods, do exercises

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,42 @@
+#Full Name Greeting
+puts "What is your first name?"
+first_name = gets.chomp
+puts "Your middle name?"
+middle_name = gets.chomp
+puts "And your last name?"
+last_name = gets.chomp
+puts "Hello " + first_name + " " + middle_name + " " + last_name + "!"
+
+#Bigger, Better Favorite Number
+puts "Tell me your favorite number."
+fav_num = gets.chomp.to_i
+puts "Why don't you try #{fav_num + 1}, instead? It's bigger and better!"
+
+
+
+=begin
+How do you define a local variable?
+
+
+
+How do you define a method?
+
+
+
+What is the difference between a local variable and a method?
+
+
+
+How do you run a ruby program from the command line?
+
+
+
+How do you run an RSpec file from the command line?
+
+
+
+What was confusing about this material? What made sense?
+
+
+
+=end
