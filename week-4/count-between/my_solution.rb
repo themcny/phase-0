e9a2c17253fd49280fd67fb7be6ed4344476fb89
unknown <n.lindsey.yee@gmail.com>
2015-09-29 15:53:17 -0700

    Count betwen numbers in a range

---
+++ week-4/count-between/my_solution.rb
@@ -0,0 +1,32 @@
+# Count Between
+
+# I worked on this challenge [by myself].
+
+# count_between is a method with three arguments:
+#   1. An array of integers
+#   2. An integer lower bound
+#   3. An integer upper bound
+#
+# It returns the number of integers in the array between the lower and upper bounds,
+# including (potentially) those bounds.
+#
+# If +array+ is empty the method should return 0
+
+# Your Solution Below
+
+def count_between(list_of_integers, lower_bound, upper_bound)
+	if list_of_integers == []
+		return 0
+	else
+		num = 0
+		i = 0
+		while i < list_of_integers.length
+			if list_of_integers[i] <= upper_bound && list_of_integers[i] >= lower_bound
+				num += 1
+			else
+			end
+			i += 1
+		end
+		return num
+	end
+end
