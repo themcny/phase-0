1c600378ec1950f1d2008bbd9c6cbfb77f11b83a
unknown <n.lindsey.yee@gmail.com>
2015-09-29 19:37:37 -0700

    Finish 4.5 exercises

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,13 @@
+# Simple Substrings
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+def welcome(string)
+	if string.include? "CA"
+		return "Welcome to California"
+	else
+		return "You should move to California"
+	end
+end
