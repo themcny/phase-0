3b3da2961223b26560f5ca16d7ee98ca869582a9
unknown <n.lindsey.yee@gmail.com>
2015-10-01 13:50:26 -0700

    Add in pseudocode

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -9,21 +9,24 @@
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input:
-# Output:
+# Input: array
+# Output: total
 # Steps to solve the problem.
-
+# total = 0
+# array.EACH do |i|
+# => total = total + i
+#end
+#RETURN total
 
 # 1. total initial solution
-=begin
-def total(array)
-	total = 0
-	array.each do |i|
-		total += i
-	end
-	return total
-end
-=end
+
+#def total(array)
+#	total = 0
+#	array.each do |i|
+#		total += i
+#	end
+#	return total
+#end
 
 # 3. total refactored solution
 def total(array)
@@ -33,23 +36,26 @@ end
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input: array
+# Output: sentence
 # Steps to solve the problem.
-=begin
-def sentence_maker(array)
-	sentence = ""
-	array[0] = array[0].capitalize
-	array.each do |i|
-		sentence = sentence + i.to_s + " "
-	end
-	return sentence.strip + "."
-end
-=end
+# setence = ""
+# array.EACH do |i|
+# => sentence = sentence + i.to_s + " "
+# end
+# RETURN sentence + "."
 
 # 5. sentence_maker initial solution
 
 
+#def sentence_maker(array)
+#	sentence = ""
+#	array[0] = array[0].capitalize
+#	array.each do |i|
+#		sentence = sentence + i.to_s + " "
+#	end
+#	return sentence.strip + "."
+#end
 
 # 6. sentence_maker refactored solution
 def sentence_maker(array)
