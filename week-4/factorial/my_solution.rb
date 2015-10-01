f79af07a4fb6d5157eafd55551b126152981d61a
unknown <n.lindsey.yee@gmail.com>
2015-10-01 13:43:56 -0700

    Add pairing exercise 4.7.1

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,9 +1,52 @@
 # Factorial
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with: Steven Broderick.
 
+
+  # ***Recursive***
+  # IF number == 0
+  # => RETURN 1
+  # ELSIF number == 1
+  # => RETURN 1
+  # ELSE for all other numbers
+  # => RETURN factorial(number - 1) * number
+  # END
+
+  # ***Non-Recursive***
+  # IF number == 0
+  # => RETURN 1
+  # ELSIF number == 1
+  # => RETURN 1
+  # ELSE
+  # => total = number
+    # WHILE number > 1
+    # => number = number - 1
+    # => total = total * number
+    # END
+  # RETURN total
+  # END
 
 # Your Solution Below
 def factorial(number)
- # Your code goes here
+
+  # *** Recursive ***
+  # if number == 0 || number == 1
+  #   return 1
+  # else
+  #   return factorial(number - 1) * number
+  # end
+
+  # *** Non-Recursive ***
+  if number == 0 || number == 1
+    return 1
+  else
+    total = number
+    while number > 1
+      number -= 1
+      total *= number
+    end
+  return total
+  end
+
+
 end
