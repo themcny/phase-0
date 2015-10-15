e8b813d7fce53146bb21f9b7ee59cfb22d2e28a0
unknown <n.lindsey.yee@gmail.com>
2015-10-14 18:53:24 -0700

    Write code for 6.4

--- week-6/attr/my_solution.rb
+++ week-6/attr/my_solution.rb
@@ -2,14 +2,13 @@
 
 # I worked on this challenge [by myself]
 
-# I spent [#] hours on this challenge.
+# I spent [1.5] hours on this challenge.
 
 class NameData
 	
 	attr_reader :name
-
 	def initialize
-		@name = 'Nicole'
+		@name = "Nicole"
 	end
 
 end
@@ -18,15 +17,18 @@ end
 class Greetings
 
 	def initialize
-		#something
+		@namedata = NameData.new
 	end
 
 	def hello
-		#something else
+		puts "Hello " + @namedata.name + "! I hope you are well today."
 	end
 
 end
 
+
+greet = Greetings.new
+greet.hello
 
 
 # Reflection
@@ -46,42 +48,78 @@ end
 #
 # How are they modifying or returning the value of instance variables?
 #
-#
+# 
 #
 
+
 # Release 2
-#
+
 # What changed between the last release and this release?
 #
-#
-#
-#
+# There is now a line 'attr_reader :age'. The entire method 'what_is_age'
+# has been taken out.
+
+
 # What was replaced?
 #
-#
-#
-#
+# The entire method 'what_is_age' has been taken out.
+
+
 # Is this code simpler than the last?
 #
-#
-#
-#
+# Yes. Rather than include the 'what_is_age' method, the 'age' can now be accessed by calling
+# instance_of_profile.age. So a three line method has been replaced by a single line attr_reader
+# variable.
+
+
 
 # Release 3
 #
 # What changed between the last release and this release?
 #
-#
+# Now there is a line 'attr_writer :age'. The entire method 'change_my_age'
+# has been deleted. 
 #
 #
 # What was replaced?
 #
-#
+# The method 'change_my_age'.
 #
 #
 # Is this code simpler than the last?
 #
+# Yes. Rather than another method, there is a single line that allows the user to change
+# the age trait.
 #
 #
-#
-#
+# 
+
+
+=begin
+
+What is a reader method?
+
+
+
+
+What is a writer method?
+
+
+
+
+What do the attr methods do for you?
+
+They allow you to return a trait without having to write a method who's only function is
+to return age or name (or whatever trait you're trying to return).
+
+
+Should you always use an accessor to cover your bases? Why or Why not?
+
+
+
+What is confusing to you about these methods?
+
+
+
+
+=end
