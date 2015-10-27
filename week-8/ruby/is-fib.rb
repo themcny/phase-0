a6c29cf8f2aba7bcd56fb36bb1d58cf76388ddeb
unknown <n.lindsey.yee@gmail.com>
2015-10-26 19:33:01 -0700

    Add week8 folder

---
+++ week-8/ruby/is-fib.rb
@@ -0,0 +1,54 @@
+# I worked on this challenge [by myself].
+# This challenge took me [#] hours.
+
+# Pseudocode
+# Input: An Integer - num
+# Output: Boolean - true or false
+# Steps:
+# Must meet one of two conditions:
+# =>  (5*num^2 + 4) == perfect square
+# =>  (5*num^2 - 4) == perfect square
+
+# To check for a perfect square -
+# sqrt_a = SQUAREROOT((5*num^2 + 4))
+# sqrt_b = SQUAREROOT((5*num^2 - 4))
+
+# IF INTEGER(sqrt_a)*INTEGER(sqrt_a) == (5*num^2 + 4) THEN
+# RETURN true
+# ELSE IF INTEGER(sqrt_b)*INTEGER(sqrt_b) == (5*num^2 - 4) THEN
+# RETURN true
+# ELSE
+# RETURN false
+# ENDIF
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+	cond_one = 5*num*num + 4
+	cond_two = 5*num*num - 4
+
+	sqrt_one = Math.sqrt(cond_one)
+	sqrt_two = Math.sqrt(cond_two)
+
+	int_one = sqrt_one.to_i
+	int_two = sqrt_two.to_i
+
+	if sqrt_one == int_one || sqrt_two == int_two
+		true
+	else
+		false
+	end
+		 	 
+end
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
