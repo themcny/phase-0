e3d856fff853802ec27943d5dfb9281e251cc9a8
Nicole Yee <n.lindsey.yee@gmail.com>
2015-11-04 14:44:23 -0800

    Commit this stupid shit before I mess it up

---
+++ week-9/ruby-review-1/ruby-review.rb
@@ -0,0 +1,48 @@
+# Introduction to Inheritance
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Pseudocode
+
+
+
+
+# Initial Solution
+
+class GlobalCohort
+  #your code here
+
+end
+
+class LocalCohort
+  #your code here
+
+end
+
+
+# Refactored Solution
+
+
+
+
+
+# Reflection
+=begin
+What concepts did you review in this challenge?
+
+Inheritance.
+
+
+What is still confusing to you about Ruby?
+
+
+
+
+What are you going to study to get more prepared for Phase 1?
+
+
+
+
+
+=end
